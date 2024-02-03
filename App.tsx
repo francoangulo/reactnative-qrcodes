3a6c83890279975b03e59c212d9f1ff3dbdd3aab
francoangulo <francoangulo2001@gmail.com>
2024-02-03 14:57:58 -0300

    QR Codes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
+import QRCode from "react-native-qrcode-svg";
+import logo from "./kisspng-paw-dog-computer-icons-5aed1ffdb797a2.054397131525489661752.png";
 
 const App = () => {
   return (
     <SafeAreaView
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <Text style={{ fontSize: 30 }}>Hello world!</Text>
+      <Text style={{ fontSize: 30, marginBottom: 50 }}>QR CODES!</Text>
+      <QRCode
+        value="QR CODE FOR PETS"
+        size={200}
+        backgroundColor="orange"
+        color="purple"
+        logo={logo}
+        logoSize={100}
+      />
     </SafeAreaView>
   );
 };
